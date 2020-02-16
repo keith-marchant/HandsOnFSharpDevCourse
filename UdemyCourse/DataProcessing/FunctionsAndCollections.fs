06aa94043431d15a478d10154eac7c2252e01841
Keith Marchant <kemarchant@outlook.com>
2020-02-17 08:25:10 +1100

    collections redux

--- UdemyCourse/DataProcessing/FunctionsAndCollections.fs
+++ UdemyCourse/DataProcessing/FunctionsAndCollections.fs
@@ -11,3 +11,50 @@ module FunctionsAndCollections =
     seq { for i in 1..3 -> i*i }
     |> Seq.iter (printfn "%d")
 
+    seq { for i in 1 .. 3 do yield i*i }
+    |> Seq.iter (printfn "%d")
+
+    seq {
+        for i in 1..3 do
+            for j in 4..5 do
+                yield i + j
+    }
+    |> Seq.iter (printfn "%d")
+
+    let rec listAllFiles path = 
+        seq {
+            for file in Directory.GetFiles(path) do
+                yield file
+            for directory in Directory.GetDirectories(path) do
+                yield! listAllFiles directory //yield! means the function evaluates into a sequence itself
+        }
+
+    listAllFiles @"C:\source\HandsOnFSharpDevCourse\UdemyCourse"
+    |> Seq.iter (printfn "%s")
+
+    
+    let readSingleLines filePath =
+        seq {
+                let rec readLine (reader:StreamReader) = 
+                    seq {
+                        if reader.EndOfStream = false
+                        then    
+                            yield reader.ReadLine()
+                            yield! readLine reader
+                    }
+
+                use stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None)
+                let reader = new StreamReader(stream)
+                yield! readLine reader
+        }
+
+
+    let lines = readSingleLines @"C:\source\HandsOnFSharpDevCourse\UdemyCourse\DataProcessing\Loan payments data.csv"
+    let enumerator = lines.GetEnumerator()
+    enumerator.MoveNext() // File is now locked
+    enumerator.Current
+    enumerator.Dispose() // Dispose and unlock the file
+
+    lines
+    |> Seq.iter (printfn "%s") // Does the above automatically so no need to explicitly call dispose
+    
---
+++ UdemyCourse/DataProcessing/FunctionsAndCollections2.fs
@@ -0,0 +1,57 @@
+﻿namespace UdemyCourse.DataProcessing
+
+module FunctionsAndCollections2 =
+    
+    1
+    |> Seq.unfold( fun n -> // unfold must contain an option function
+        if n < 300
+            then Some(n, n + n) // must be a tuple containing the current and new values
+            else None)
+    |> Seq.iter (printfn "%d ")
+
+    (1,0)
+    |> Seq.unfold (fun (n1, n2) -> Some(n1 + n2, (n2, n1 + n2))) // fibonacci
+    |> Seq.take 15
+    |> Seq.iter (printfn "%d ")
+
+    let someNumbers = [ 1..5 ]
+    let moreNumbers = [ 6..10 ]
+    let combinedNumbers = List.append someNumbers moreNumbers
+
+    let squareAndCube n = 
+        [
+            n + n
+            n * n * n
+        ]
+
+    combinedNumbers
+    |> List.collect squareAndCube // expand each element into a new collection
+    |> List.iter (printfn "%d ")
+
+    let zippedList = List.zip someNumbers moreNumbers // create tuples of the elements of the two lists
+    let unzippedList = List.unzip zippedList
+
+    let stillMoreNumbers = [ 11..15 ]
+    let zipped3List = List.zip3 someNumbers moreNumbers stillMoreNumbers
+    let unzipped3List = List.unzip3 zipped3List
+
+    let notZippedList = List.zip someNumbers combinedNumbers // won't work, lists aren't the same length
+    let zippedSeq = Seq.zip someNumbers combinedNumbers // works, limited to the smallest list
+
+    someNumbers |> List.pairwise
+    someNumbers |> List.windowed 2
+    someNumbers |> List.windowed 3
+
+    let petBreeds = 
+        [
+            ("Cat", "Persian")
+            ("Dog", "Collie")
+            ("Cat", "Russian Blue")
+            ("Bird", "Canary")
+            ("Dog", "Corgie")
+            ("Cat", "Siamese")
+        ]
+
+    petBreeds
+    |> List.groupBy (fun t -> fst t)
+
