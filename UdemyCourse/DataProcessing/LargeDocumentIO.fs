18dfcfbb5291e200a4a8bd16cb548ff75204ac10
Keith Marchant <kemarchant@outlook.com>
2020-02-18 20:53:54 +1100

    Module 2 cont.

---
+++ UdemyCourse/DataProcessing/LargeDocumentIO.fs
@@ -0,0 +1,28 @@
+﻿namespace UdemyCourse.DataProcessing
+
+module LargeDocumentIO = 
+
+    open System
+    open System.IO
+    open System.Collections.Generic
+
+    (*
+    #time
+    *)
+
+    let f = 
+        fun () -> 
+            for i in [ 1..100 ] do printfn "%d" i
+
+    f()
+
+    let timeAFunction func =
+        let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+        let result = func()
+        stopwatch.Stop()
+        (stopwatch.ElapsedMilliseconds, result)
+
+    timeAFunction f
+
+
+
